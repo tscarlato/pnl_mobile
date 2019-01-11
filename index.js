d1d549eb39f5533f8b6545979383f04342c2f4fa
tscarlato <tscarlato@gmail.com>
2019-01-11 18:46:21 -0500

    This version has the caret, doors as a sub unit of locations a header and nothing else. It's the skeleton of the app

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { makeMainRoutes } from './routes';
+import { AppRegistry } from 'react-native';
+AppRegistry.registerComponent('pnl_mobile', () => App);
 
 const routes = makeMainRoutes();
 
--- screens/locations.js
+++ screens/locations.js
@@ -1,33 +1,136 @@
 import React, { Component } from 'react';
-import Doors from './Doors';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {
-    Button,
-    Image,
-    Platform,
-    ScrollView,
     StyleSheet,
     Text,
     TouchableOpacity,
     View,
+    Switch,
 } from 'react-native';
 
 export default class Locations extends Component {
 
+    constructor(props) {
+        super(props);
+        this.showDoors = this.showDoors.bind(this);
+        this.toggleShowDoors = this.toggleShowDoors.bind(this);
+        this.state = {
+            showDoors: false
+          };
+       }
+
+    toggleShowDoors() {
+        this.setState({showDoors:!this.state.showDoors})
+      }
+
+    showDoors() {
+        //returns a list of all songs on an album if caret is toggled
+        if (!this.state.showDoors){
+          return
+        }
+        // The <Text> will come from an API Call for doors. 
+        //list will be a flatlist.
+        let list =
+          <View style={styles.doorbox}>
+                
+                <Text style={styles.doortext}> This is a door</Text>
+                <Switch />
+
+
+          </View>;
+          return list
+      }
+
+
+
+      displayLocations(){
+          let locationsList = 
+          <View  style={styles.locationbox}> 
+          <TouchableOpacity onPress={this.toggleShowDoors}>
+               <View style={styles.locationpress}>
+               {this.showOpenCloseIcon()}
+                   <Text style={styles.locationtext}> This is a location</Text> 
+                   
+               </View>
+           </TouchableOpacity>
+               {this.showDoors()}                    
+
+           </View>;
+                return locationsList;
+      }
+
+
+      showOpenCloseIcon() {
+        if(this.state.showDoors)
+        {
+          return <Icon name="caret-down" size={25} color='#808080' />
+        }
+        else
+        {
+          return <Icon name="caret-right" size={25} color='#808080' />
+        }
+      }
+
 
     render() {
-        const { navigate } = this.props.navigation;
-
         return (
-            <View>
-            <Text> PNL </Text >
-            <Button
-                    title="Go to Doors'"
-                    onPress={() => navigate('Doors')}
+            <View style={styles.mainpage}>
+                <View style={styles.header}>
+                <Text style={styles.headertext}> Locations </Text >
+               </View>
 
-            />
-                    
+            {this.displayLocations()} 
+
             </View>
         );
     }
 };
+
+const styles = StyleSheet.create({
+
+    mainpage:{
+        padding: 5,
+    },
+
+    header:{ 
+        alignItems: 'center',
+        paddingTop: 25,
+        paddingBottom: 25,     
+    },
+
+    headertext:{ 
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#bb33bb',
+    },
+
+    locationbox:{ 
+        paddingTop: 10,
+        paddingBottom: 10,  
+    },
+
+    locationtext:{ 
+        fontSize: 21,
+        fontWeight: 'bold',
+        color: '#7733bb',
+    },
+
+    locationpress:{ 
+        flexDirection: 'row',
+    },
+
+    doorbox:{ 
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    doortext:{ 
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#773355',
+    },
+
+});
